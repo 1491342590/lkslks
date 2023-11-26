99615529a34cb93ef138adbbf631fb526301a48a
小蜗牛 <128204523+1491342590@users.noreply.github.com>
2023-11-26 12:57:42 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST数据包POC</title>
+</head>
+<body>
+<form action="http://99da1d95-7eb6-468a-b044-cf1b1b5b393d.challenge.ctf.show/" method="post" enctype="multipart/form-data">
+    <!--目标网址-->
+    <label for="file">文件名：</label>
+    <input type="file" name="file" id="file"><br>
+    <input type="submit" name="submit" value="提交">
+</form>
+</body>
+</html>
